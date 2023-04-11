a488267e19cd56010e97bad1c93770ea7f5a174d
Andrea Colli-Vignarelli <andreacv1998@gmail.com>
2023-04-11 19:09:35 +0200

    minor sync fix and liqo kind unpeer fix

--- example-liqo/scripts/enviroment.sh
+++ example-liqo/scripts/enviroment.sh
@@ -9,7 +9,7 @@ sudo sysctl fs.inotify.max_user_instances=512
 echo "Creating kind clusters..."
 echo "Creating service-provider cluster..."
 # create a cluster with the local registry enabled in containerd
-cat <<EOF | kind create cluster --name="service-provider" --kubeconfig="service-provider.kconf" 
+cat <<EOF | kind create cluster --name="service-provider" --kubeconfig="service-provider.kconf" --image "kindest/node:v1.25.3"
 EOF
 
 echo "Installing CRDs..."
@@ -19,28 +19,28 @@ EOF
 
 echo "Creating customer cluster..."
 # create the second cluster
-cat <<EOF | kind create cluster --name="customer" --kubeconfig="customer.kconf"
+cat <<EOF | kind create cluster --name="customer" --kubeconfig="customer.kconf" --image "kindest/node:v1.25.3"
 EOF
 
 echo "Creating customer2 cluster..."
 # create the third cluster
-cat <<EOF | kind create cluster --name="customer2" --kubeconfig="customer2.kconf"
+cat <<EOF | kind create cluster --name="customer2" --kubeconfig="customer2.kconf" --image "kindest/node:v1.25.3"
 EOF
 
 echo "Installing liqo..."
 echo "Installing liqo on service-provider cluster..."
 # install liqo on first cluster
-cat <<EOF | liqoctl install kind --cluster-name service-provider --kubeconfig="service-provider.kconf"
+cat <<EOF | liqoctl install kind --cluster-name service-provider --kubeconfig="service-provider.kconf" --version="v0.7.0"
 EOF
 
 echo "Installing liqo on customer cluster..."
 # install liqo on second cluster
-cat <<EOF | liqoctl install kind --cluster-name customer --kubeconfig="customer.kconf"
+cat <<EOF | liqoctl install kind --cluster-name customer --kubeconfig="customer.kconf" --version="v0.7.0"
 EOF
 
 echo "Installing liqo on customer2 cluster..."
 # install liqo on thirds cluster
-cat <<EOF | liqoctl install kind --cluster-name customer2 --kubeconfig="customer2.kconf"
+cat <<EOF | liqoctl install kind --cluster-name customer2 --kubeconfig="customer2.kconf" --version="v0.7.0"
 EOF
 
 echo "Liqo peering commands:"
